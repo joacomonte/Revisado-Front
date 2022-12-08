0aa40155cd90bbc06ca36d09673b888607ec35b8
Joaquin Montesano <montesano1994@gmail.com>
2022-12-08 16:28:54 -0300

    prueba4

--- src/index.js
+++ src/index.js
@@ -3,23 +3,27 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import {configureStore} from '@reduxjs/toolkit'
-import {Provider} from 'react-redux'
 
 
-const store = configureStore(
-    {
-        reducer:{}
-    }
-)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <Provider store={store}>
+
             <App />
-        </Provider>
+
     </BrowserRouter>
 
 );
 
+{/* <Provider store={store}>
+</Provider>
+import {configureStore} from '@reduxjs/toolkit'
+import {Provider} from 'react-redux' */}
+
+
+// const store = configureStore(
+//     {
+//         reducer:{}
+//     }
+// )
